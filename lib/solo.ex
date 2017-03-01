cb328c29e603cb1fb5accb89c2a9a5e1a97e193b
aequanimitas <htavenido@gmail.com>
2017-03-01 21:36:23 +0800

    shamelessly steal iex code

--- lib/solo.ex
+++ lib/solo.ex
@@ -1,26 +1,53 @@
 defmodule Solo do
-  @moduledoc """
-  Documentation for Solo.
-  """
+  @moduledoc false
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Solo.hello
-      :world
-
+  Initialize processes here, called only once to avoid spawning new processes
   """
-
-  import Plug.Conn
-
-  def init(options) do
-    options
+  def main(_args \\ []) do
+    self_pid = self()
+    pid = spawn_link(fn -> input_loop(self_pid) end)
+    do_loop(pid)
   end
-  def call(conn, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello Solo")
+
+  @doc """
+  Imagine this as an intermediate step so that the two processes (input_loop and wait_input
+  can start send messages to each other. The problem when you do a send in main is that 
+  you're creating a new process everytime main is called. Just do this mediary function
+  """
+  def do_loop(pid) do
+    send pid, {:do_input}
+    wait_input(pid)
+  end
+
+  @doc """
+  Handle messages coming from the input_loop 
+  """
+  def wait_input(pid) do
+    receive do
+      {:input, input} ->
+        IO.write "(#{format_date_today(:calendar.local_time())}) Message received: #{input}"
+        do_loop(pid)
+    end
+  end
+
+  defp format_date_today(_dte={{year,month,day}, {hour,minute,second}}) do
+    "#{year}/#{month}/#{day} #{hour}:#{minute}:#{second}"
+  end
+
+  @doc """
+  Loop forever until eof
+  """
+  def input_loop(pid) do
+    receive do
+      {:do_input} ->
+        input = case IO.gets(:stdio, "Waiting for braindump: ") do
+          :eof -> :eof
+          {:error, _ } -> ""
+          data -> data
+        end
+        send pid, {:input, input}
+    end
+    input_loop(pid)
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,9 +7,13 @@ defmodule Solo.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     escript: escript()]
   end
 
+  def escript do
+    [main_module: Solo]
+  end
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
